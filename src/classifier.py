1bcc3ea56c10df87d076055f573825c868475103
Kerstin <kerstin@github.com>
2017-11-19 17:37:56 +0100

    KNN

--- src/classifier.py
+++ src/classifier.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import io_util as io
+from sklearn.neighbors import KNeighborsClassifier
 
 def naive_bayes(dataset):
     '''Trains and tests a Naive Bayes Classifier with selected features'''
+    ###können wir das nicht für alle methoden verwenden bis zu data_train, data_test,...?
     from sklearn.naive_bayes import GaussianNB
     from sklearn.model_selection import train_test_split
     from category_encoders.ordinal import OrdinalEncoder
     from sklearn.metrics import accuracy_score
-
+    
     encoder = OrdinalEncoder()
     dataset_encoded = encoder.fit_transform(dataset)
     listings_data=dataset_encoded.drop(columns=['id','perceived_quality'])
@@ -29,6 +31,13 @@ def naive_bayes(dataset):
 dataset = io.read_csv('../data/playground/dataset.csv')
 naive_bayes(dataset)
 
-def knn(dataset):
+def knn(dataset, knn_estimator, data_train, target_train, data_test, target_test):
     '''KNN'''
-    
+    knn_estimator = KNeighborsClassifier(4)
+    knn_estimator.fit(data_train, target_train)
+
+    predict = knn_estimator.predict(data_test)
+    print('Prediction of KNN Classifier:{}'.format(predict))
+
+    accuracy = knn_estimator.score(target_test, predict)
+    print('Accuracy of KNN Classifier:{}'.format(accuracy))
